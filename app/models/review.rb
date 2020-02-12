9491937b6438dbac2e8860e7227e7e7fead6c28b
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-02-12 17:17:56 +0100

    RESTO-REVIEWS DONE

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
+  RATES = [0, 1, 2, 3, 4, 5]
   validates :content, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: RATES }
 end
